963f11e4b719b3e62d063b67475b68a5a8163b0b
hjtm130 <osechi130uto@gmail.com>
2024-08-28 15:09:06 +0900

    [編集] 各ページへのroutes

--- testapp/routes.py
+++ testapp/routes.py
@@ -4,3 +4,18 @@ from testapp import app
 @app.route('/')
 def index():
     return render_template('Home.html')
+@app.route('/AIChat')
+def index():
+    return render_template('AIChat.html')
+@app.route('/CounselorChat')
+def index():
+    return render_template('CounselorChat.html')
+@app.route('/Tips')
+def index():
+    return render_template('Tips.html')
+@app.route('/JuniorHighSchool')
+def index():
+    return render_template('JuniorHighSchool.html')
+@app.route('/HighSchool')
+def index():
+    return render_template('HighSchool.html')

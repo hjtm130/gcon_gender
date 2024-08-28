783b990810ec6a0d6390a198fb34a6261c1ed9de
hjtm130 <osechi130uto@gmail.com>
2024-08-28 15:34:53 +0900

    [編集] routeバグ修正

--- testapp/routes.py
+++ testapp/routes.py
@@ -4,18 +4,23 @@ from testapp import app
 @app.route('/')
 def index():
     return render_template('Home.html')
+
 @app.route('/AIChat')
-def index():
+def ai_chat():
     return render_template('AIChat.html')
+
 @app.route('/CounselorChat')
-def index():
+def counselor_chat():
     return render_template('CounselorChat.html')
+
 @app.route('/Tips')
-def index():
+def tips():
     return render_template('Tips.html')
+
 @app.route('/JuniorHighSchool')
-def index():
+def junior_high_school():
     return render_template('JuniorHighSchool.html')
+
 @app.route('/HighSchool')
-def index():
+def high_school():
     return render_template('HighSchool.html')
